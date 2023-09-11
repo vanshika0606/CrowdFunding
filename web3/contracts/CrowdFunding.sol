32196d79a3290e4b8a4f067ad3cfa564d5497318
Vanshika <vanshika06065@gmail.com>
2023-09-11 12:45:06 +0530

    implemented create campaign contract

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -7,7 +7,7 @@ contract MyContract {
         string title;
         string description;
         uint256 target;
-        uint256 deadlin;
+        uint256 deadline;
         uint256 amountCollected;
         string image;
         address[] donators;
@@ -17,5 +17,35 @@ contract MyContract {
     mapping(uint256 => Campaign)  public campaigns;
 
     uint256 public numberOfCampaigns = 0;
+
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
+        Campaign storage compaign = compaigns[numberOfCampaigns];
+
+        require(compaign.deadline < block.timestamp, "The deadline should be a date in futute");
+
+        compaign.owner = _owner;
+        compaign.title = _title;
+        compaign.description = _description;
+        compaign.target = _target;
+        compaign.deadline = _deadline;
+        compaign.amountCollected = 0;
+        compaign.image = _image;
+        
+        numberOfCampaigns++;
+
+        return numberOfCampaigns-1;
+    }
+
+    function donateTocompaign (uint256 _id) public payable {
+
+    }
+
+    function getDonators() {
+
+    }
+
+    function getCompaigns() {
+
+    }
     constructor() {}
 }
