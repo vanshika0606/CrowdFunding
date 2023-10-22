89447d9e7ee6610d9bd364ae3d8e2196755cc753
Tanvi <tanvitomar592@gmail.com>
2023-10-22 23:55:47 +0530

    Log out functionality added

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-
 import { logo, sun } from "../assets";
+import LogOut from "./LogOut";
 import { navlinks } from "../constants";
+import { useDisconnect } from "@thirdweb-dev/react";
 
 const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
   <div
@@ -28,33 +29,49 @@ const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
 const Sidebar = () => {
   const navigate = useNavigate();
   const [isActive, setIsActive] = useState("dashboard");
+  const [isLogout, setIsLogout] = useState(false);
+  const disconnect = useDisconnect();
+
+  const disconnectfun = () => {
+    setIsLogout(true);
+    console.log("jhj");
+    disconnect();
+    console.log("jhj");
+    setIsLogout(false);
+  };
 
   return (
-    <div className="flex justify-between items-center flex-col sticky top-5 h-[93vh]">
-      <Link to="/">
-        <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} />
-      </Link>
+    <>
+      {isLogout && <LogOut />}
+      <div className="flex justify-between items-center flex-col sticky top-5 h-[93vh]">
+        <Link to="/">
+          <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} />
+        </Link>
 
-      <div className="flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-12">
-        <div className="flex flex-col justify-center items-center gap-3">
-          {navlinks.map((link) => (
-            <Icon
-              key={link.name}
-              {...link}
-              isActive={isActive}
-              handleClick={() => {
-                if (!link.disabled) {
-                  setIsActive(link.name);
-                  navigate(link.link);
-                }
-              }}
-            />
-          ))}
+        <div className="flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-12">
+          <div className="flex flex-col justify-center items-center gap-3">
+            {navlinks.map((link) => (
+              <Icon
+                key={link.name}
+                {...link}
+                isActive={isActive}
+                handleClick={() => {
+                  if (!link.disabled) {
+                    setIsActive(link.name);
+                    navigate(link.link);
+                  } else if (link.name == "logout") {
+                    setIsActive(link.name);
+                    disconnectfun();
+                  }
+                }}
+              />
+            ))}
+          </div>
+
+          <Icon styles="bg-[#1c1c24] shadow-secondary" imgUrl={sun} />
         </div>
-
-        <Icon styles="bg-[#1c1c24] shadow-secondary" imgUrl={sun} />
       </div>
-    </div>
+    </>
   );
 };
 
--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -5,7 +5,7 @@ import {
   useContract,
   useMetamask,
   useContractWrite,
-  useContractRead
+  useContractRead,
 } from "@thirdweb-dev/react";
 import { ethers } from "ethers";
 
@@ -19,14 +19,12 @@ export const StateContextProvider = ({ children }) => {
     contract,
     "createCampaign"
   );
-  const { data, isLoading } = useContractRead(contract, "getcampaigns")
+  const { data, isLoading } = useContractRead(contract, "getcampaigns");
   // console.log("data : ", data);
   // console.log("isLoading: ", isLoading);
 
-
   const address = useAddress();
   const connect = useMetamask();
-
 
   const publishCampaign = async (form) => {
     try {
@@ -47,65 +45,80 @@ export const StateContextProvider = ({ children }) => {
   };
 
   const getCampaigns = async () => {
-    
-      if(!isLoading){
-        console.log(data);
-      }
-    
+    if (!isLoading) {
+      console.log(data);
+    }
+
     const parsedCampaings = data?.map((campaign, i) => ({
       owner: campaign.owner,
       title: campaign.title,
       description: campaign.description,
       target: ethers.utils.formatEther(campaign.target.toString()),
       deadline: campaign.deadline.toNumber(),
-      amountCollected: ethers.utils.formatEther(campaign.amountCollected.toString()),
+      amountCollected: ethers.utils.formatEther(
+        campaign.amountCollected.toString()
+      ),
       image: campaign.image,
-      pId: i
+      pId: i,
     }));
 
     // console.log(parsedCampaings);
 
     return parsedCampaings;
-  }
+  };
 
   const getUserCampaigns = async () => {
     const allCampaigns = await getCampaigns();
 
-    const filteredCampaigns = allCampaigns.filter((campaign) => campaign.owner === address);
+    const filteredCampaigns = allCampaigns.filter(
+      (campaign) => campaign.owner === address
+    );
 
     return filteredCampaigns;
-  }
+  };
 
   const donate = async (pId, amount) => {
-    const data = await contract.call('donateTocampaign', [pId], { value: ethers.utils.parseEther(amount)});
+    const data = await contract.call("donateTocampaign", [pId], {
+      value: ethers.utils.parseEther(amount),
+    });
 
     return data;
-  }
+  };
 
   const getDonations = async (pId) => {
-    const donations = await contract.call('getDonators', [pId]);
+    const donations = await contract.call("getDonators", [pId]);
     const numberOfDonations = donations[0].length;
 
     const parsedDonations = [];
 
-    for(let i = 0; i < numberOfDonations; i++) {
+    for (let i = 0; i < numberOfDonations; i++) {
       parsedDonations.push({
         donator: donations[0][i],
-        donation: ethers.utils.formatEther(donations[1][i].toString())
-      })
+        donation: ethers.utils.formatEther(donations[1][i].toString()),
+      });
     }
 
     return parsedDonations;
-  }
-
+  };
 
   return (
     <StateContext.Provider
-      value={{ address, contract, connect, createCampaign: publishCampaign, getCampaigns, getUserCampaigns, isLoading, data, getDonations, donate}}
+      value={{
+        address,
+        contract,
+        connect,
+        createCampaign: publishCampaign,
+        getCampaigns,
+        getUserCampaigns,
+        isLoading,
+        data,
+        getDonations,
+        donate,
+      }}
     >
-       {children}
+      {children}
     </StateContext.Provider>
   );
 };
 
-export const useStateContext = ()=> useContext(StateContext);
+export const useStateContext = () => useContext(StateContext);
